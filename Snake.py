b5daf956797b7f88755d2a88db2daabdf2b7e87e
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2020-04-11 23:42:47 +0200

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -22,12 +22,6 @@ pygame.display.update()
 pygame.display.set_caption('Snake by Xabier Gabiña')
 
 # Inicializacion del personaje
-x1=display_w/2
-y1=display_h/2
-
-x1_change=0
-y1_change=0
-
 snake_block=10  # Tamaño del jugador
 snake_speed=30  # Velocidad del juegador
 
@@ -49,50 +43,83 @@ def message(msg,color):
 
 #============================================================================================================================================================================================================================================#
 # Logica del juego
-game_over=False
 
-while not game_over:
-    for event in pygame.event.get():    # Se registra cualquier cambio, ya sea de raton, teclado o en la pantalla
-        if event.type==pygame.QUIT:     # Se activa cuando se pulsa el boton de cerrar
+def gameLoop():
+    game_over=False
+    game_close=False
+
+    x1=display_w/2
+    y1=display_h/2
+
+    x1_change=0
+    y1_change=0
+
+    foodx=round(random.randrange(0, display_w - snake_block)/10.0)*10.0
+    foody=round(random.randrange(0,display_h-snake_block)/10.0)*10.0
+
+    while not game_over:
+
+        while game_close == True:
+            display.fill(white)
+            message("¡Has perdido! Presiona Q para salir o R para jugar de nuevo", red)
+            pygame().display.update()
+            
+            for event in pygame.event.get():
+                if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_q:
+                        game_over=True
+                        game_close=False
+                    if event.key==pygame.K_r:
+                        gameLoop()
+
+
+        for event in pygame.event.get():    # Se registra cualquier cambio, ya sea de raton, teclado o en la pantalla
+            if event.type==pygame.QUIT:     # Se activa cuando se pulsa el boton de cerrar
+                game_over=True
+            if event.type==pygame.KEYDOWN:  # Se activa cuando se detecta la pulsacion de una tecla
+                # Logica del movimiento de la serpiente
+                if event.key == pygame.K_LEFT:
+                    x1_change = -10
+                    y1_change = 0
+                if event.key == pygame.K_RIGHT:
+                    x1_change = 10
+                    y1_change = 0
+                if event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -10
+                if event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = 10
+
+        # Condicion de derrota - Salirse del mapa
+        if x1>=display_w or x1<0 or y1>=display_h or y1<0:
             game_over=True
-        if event.type==pygame.KEYDOWN:  # Se activa cuando se detecta la pulsacion de una tecla
-            # Logica del movimiento de la serpiente
-            if event.key == pygame.K_LEFT:
-                x1_change = -10
-                y1_change = 0
-            if event.key == pygame.K_RIGHT:
-                x1_change = 10
-                y1_change = 0
-            if event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -10
-            if event.key == pygame.K_DOWN:
-                x1_change = 0
-                y1_change = 10
 
-    # Condicion de derrota - Salirse del mapa
-    if x1>=display_w or x1<0 or y1>=display_h or y1<0:
-        game_over=True
+        # Actualizacion del movimiento del juegador
+        x1 += x1_change
+        y1 += y1_change
+        
+        # Rellena el tablero de juego
+        display.fill(white)
 
-    # Actualizacion del movimiento del juegador
-    x1 += x1_change
-    y1 += y1_change
+        # Dibuja la comida
+        pygame.draw.rect(display, blue, [foodx, foody, snake_block, snake_block])
+
+        # Dibuja al jugador
+        pygame.draw.rect(display, black, [x1,y1,10,10])
+
+        # Actualiza en cada vuelta del while los graficos
+        pygame.display.update()
+
+        # Logica del comer
+        if x1==foodx and y1==foody:
+            print("Sabroso!")
+
+        # Velocidad del jugador
+        clk.tick(snake_speed)
     
-    # Rellena el tablero de juego
-    display.fill(white)
+    # Cerrar todo
+    pygame.quit()
+    quit()
 
-    # Dibuja al jugador
-    pygame.draw.rect(display, black, [x1,y1,10,10])
-
-    pygame.display.update()
-
-    clk.tick(snake_speed)
-
-#Imprime el mensaje de derrota
-message("Has perdido", red)
-pygame.display.update()
-time.sleep(2)
-
-# Cerrar todo
-pygame.quit()
-quit()
+gameLoop()
